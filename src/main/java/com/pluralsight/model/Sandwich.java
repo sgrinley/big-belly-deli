2c64287cf8d7ccf8ab10cf52c86ddb7af156a12d
sgrinley <shamar808@yahoo.com>
2026-05-22 07:52:08 -0700

    feat: Add lists of toppings

--- src/main/java/com/pluralsight/model/Sandwich.java
+++ src/main/java/com/pluralsight/model/Sandwich.java
@@ -1,11 +1,25 @@
 package com.pluralsight.model;
 
+import java.util.List;
+
 public class Sandwich {
 
     //    Add Data Fields
     private String size; // 4", 8", 12"
     private String breadType; // White, Wheat, Rye, Wrap, Gluten Free (GF)
     private boolean isToasted;
+
+    //    Lists of Toppings
+    private List<String> meatToppings;
+    private List<String> cheeseToppings;
+    private List<String> regularToppings;
+    private List<String> premiumToppings;
+    private List<String> sauces;
+
+    private boolean extraMeat;
+    private boolean extraCheese;
+    private boolean extraSauce;
+
 
     //    Generate Constructor
     public Sandwich(String size, String breadType, boolean isToasted) {
