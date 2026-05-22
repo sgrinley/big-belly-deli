06d789dfb8b3cae77f56030fd57639b01f6c16e7
sgrinley <shamar808@yahoo.com>
2026-05-22 10:02:25 -0700

    feat: Add order collection entities

--- src/main/java/com/pluralsight/OrderItem.java
+++ src/main/java/com/pluralsight/OrderItem.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderItem {
+    private List<Orderable> items;
+
+    public void Order() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(Orderable item) {
+        this.items.add(item);
+    }
+
+    public List<Orderable> getItems() {
+        return items;
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (Orderable item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    public void clear() {
+        items.clear();
+    }
 }
