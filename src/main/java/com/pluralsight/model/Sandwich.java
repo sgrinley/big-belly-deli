fa470a354ca951544f787cb8081c655418f9cbf3
sgrinley <shamar808@yahoo.com>
2026-05-22 07:45:48 -0700

    feat: Add Instance Variables

--- src/main/java/com/pluralsight/model/Sandwich.java
+++ src/main/java/com/pluralsight/model/Sandwich.java
@@ -1,4 +1,9 @@
 package com.pluralsight.model;
 
 public class Sandwich {
+
+    //    Add Data Fields
+    private String size; // 4", 8", 12"
+    private String breadType; // White, Wheat, Rye, Wrap, Gluten Free (GF)
+    private boolean isToasted;
 }
