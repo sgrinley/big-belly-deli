e0ae1809ddff7ba101686ad3ed5ad38ef8bd15bc
sgrinley <shamar808@yahoo.com>
2026-05-22 09:59:49 -0700

    feat: Setup chips class

--- src/main/java/com/pluralsight/model/Chips.java
+++ src/main/java/com/pluralsight/model/Chips.java
@@ -1,4 +1,21 @@
 package com.pluralsight.model;
 
-public class Chips {
+import com.pluralsight.Orderable;
+
+public class Chips implements Orderable {
+    private String type; // Regular, BBQ, Sour Cream
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.50; // Flat rate for chips as per requirements
+    }
+
+    @Override
+    public String getDetails() {
+        return type + " Chips";
+    }
 }
