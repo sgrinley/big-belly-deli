7a7557652485c0ca829e83af8754a63f6b922746
sgrinley <shamar808@yahoo.com>
2026-05-26 08:21:01 -0700

    refactor App: completed code to run test

--- src/main/java/com/pluralsight/deli/App.java
+++ src/main/java/com/pluralsight/deli/App.java
@@ -1,7 +1,10 @@
 package com.pluralsight.deli;
 
+import com.pluralsight.deli.ui.UserInterface;
+
 public class App {
     public static void main(String[] args) {
-
+        UserInterface app = new UserInterface();
+        app.run();
     }
 }
